e56fc8fd2abd4613cf163e6f7be06c1fea6e4e44
CoderGuy614 <jlutz.110@gmail.com>
2020-11-17 08:40:42 -0500

    Add Http Service and make request

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,20 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ContactFormComponent } from './contact-form/contact-form.component';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { PostsComponent } from './posts/posts.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContactFormComponent, 
+    ContactFormComponent,
+    PostsComponent, 
 
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
